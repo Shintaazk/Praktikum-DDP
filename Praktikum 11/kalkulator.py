4b855081b29792d8d5ae0d02d63a2a3a88c2220c
shinta nur allyani <146152430+Shintaazk@users.noreply.github.com>
2023-12-05 20:07:55 +0700

    Create kalkulator.py

---
+++ Praktikum 11/kalkulator.py
@@ -0,0 +1,11 @@
+import math
+
+def tambah(bil1, bil2): 
+    hasil= bil1 + bil2 
+    print('penjumlahan',bil1, "+",bil2,'=',hasil)
+
+def pangkat(bilangan1,pangkat):
+    print('pangkat',bilangan1,'^',pangkat,'adalah',math.pow(2,3))
+
+def sin(bil):
+    print('sin dari',bil,'adalah',math.sin(bil))
