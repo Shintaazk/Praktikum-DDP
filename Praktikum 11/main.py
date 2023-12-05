fd61cc532b2367ba253b8422d167289011bd8306
shinta nur allyani <146152430+Shintaazk@users.noreply.github.com>
2023-12-05 20:06:54 +0700

    Create main.py

---
+++ Praktikum 11/main.py
@@ -0,0 +1,14 @@
+import bangun_datar
+ 
+
+bangun_datar.luas_persegi(8)
+bangun_datar.luas_persegi_panjang(7,4)
+bangun_datar.luas_belah_ketupat(8,6)
+bangun_datar.luas_jajar_genjang(9,7)
+bangun_datar.luas_segitiga(8,12)
+
+import kalkulator
+
+kalkulator.tambah(6 , 7)
+kalkulator.pangkat(3 , 6)
+kalkulator.sin(8)
