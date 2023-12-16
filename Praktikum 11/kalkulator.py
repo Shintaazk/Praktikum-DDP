821ca7dd73495a4d40dc2b4f5b4147232bb6979f
shinta nur allyani <146152430+Shintaazk@users.noreply.github.com>
2023-12-16 16:05:17 +0700

    kalkulator.py

--- Praktikum 11/kalkulator.py
+++ Praktikum 11/kalkulator.py
@@ -1,11 +1,50 @@
+
 import math
 
-def tambah(bil1, bil2): 
-    hasil= bil1 + bil2 
-    print('penjumlahan',bil1, "+",bil2,'=',hasil)
+#tambah
+def tambah(bil1, bil2):
+    hasil = bil1 + bil2
+    print("hasil tambah dari",bil1,"+",bil2,"=",hasil)
 
-def pangkat(bilangan1,pangkat):
-    print('pangkat',bilangan1,'^',pangkat,'adalah',math.pow(2,3))
+#kurang
+def kurang(bil1, bil2):
+    hasil = bil1 - bil2
+    print("hasil pengurangan dari",bil1,"-",bil2,"=",hasil)
 
-def sin(bil):
-    print('sin dari',bil,'adalah',math.sin(bil))
+#kali
+def kali(bil1, bil2):
+    hasil = bil1 * bil2
+    print("hasil perkalian dari",bil1,"*",bil2,"=",hasil)
+
+#bagi
+def bagi(bil1, bil2):
+    hasil = bil1 / bil2
+    print("hasil bagi dari",bil1,"/",bil2,"=",hasil)
+
+#Pangkat
+def Pangkat(angka1,angka2):
+    hasil = (angka1 ** angka2)
+    print("Hasil Pangkat dari",angka1,"**",angka2,"=",hasil)
+
+
+#Sin
+def Sin(bil):
+    print ('Sin dari',bil,'adalah',math.sin(bil))
+
+#Cos
+def Cos(bil):
+    print ('Cos dari',bil,'adalah',math.cos(bil))
+
+#Tan
+def Tan(bil):
+    print ('Tan dari',bil,'adalah',math.tan(bil))
+
+#Log
+def Log(bil):
+    print ('Log dari',bil,'adalah',math.log(bil))
+
+#Akar
+def Akar(bil):
+    print ('Akar dari',bil,'adalah',math.sqrt(bil))
+
+
