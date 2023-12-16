10a4a839a627278246a9a1cde69410936f11affc
shinta nur allyani <146152430+Shintaazk@users.noreply.github.com>
2023-12-16 16:06:20 +0700

    main.py

--- Praktikum 11/main.py
+++ Praktikum 11/main.py
@@ -1,6 +1,5 @@
 import bangun_datar
  
-
 bangun_datar.luas_persegi(8)
 bangun_datar.luas_persegi_panjang(7,4)
 bangun_datar.luas_belah_ketupat(8,6)
@@ -10,5 +9,14 @@ bangun_datar.luas_segitiga(8,12)
 import kalkulator
 
 kalkulator.tambah(6 , 7)
-kalkulator.pangkat(3 , 6)
-kalkulator.sin(8)
+kalkulator.kurang(6 , 7)
+kalkulator.kali(6 , 7)
+kalkulator.bagi(7 , 7)
+kalkulator.Pangkat(5,5)
+kalkulator.Sin(9)
+kalkulator.Cos(9)
+kalkulator.Tan(9)
+kalkulator.Log(9)
+kalkulator.Akar(9)
+
+
